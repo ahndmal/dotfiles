2c85c7ef003d5da4b74f95f8cc5286308a9ebb60
malandr <quadr988@gmail.com>
2022-08-24 15:03:45 +0300

    vim

--- nvim/.config/nvim/lua/user/nvim-tree.lua
+++
@@ -1,8 +0,0 @@
-require('nvim-tree').setup{
-    disable_netrw = true,
-    open_on_tab = false,
-    view = {
-        side = "right",
-        width = 40,
-    }
-}
--- nvim/.config/nvim/lua/vimrc.lua
+++
@@ -1,7 +0,0 @@
-local home = os.getenv("HOME")
-local vimrc_path = home.."/.config/nvim/.vimrc"
-local vimfile = io.open(vimrc_path, "rb")
-local vimsettings = vimfile:read "*a"
-vimfile:close()
-vim.cmd(vimsettings)
-
