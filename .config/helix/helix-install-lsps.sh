656e11156d71c30999ce4e635733defcf9d304f5
andrii <quadr988@gmail.com>
2023-07-06 17:18:50 +0300

    helix

---
+++ .config/helix/helix-install-lsps.sh
@@ -0,0 +1,17 @@
+
+# java
+wget https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.25.0/jdt-language-server-1.25.0-202306291518.tar.gz
+tar xf jdt-language-server-1.25.0-202306291518.tar.gz
+./jdt-language-server-1.25.0-202306291518/bin/jdtls
+
+# haskell
+ghcup install hls
+
+# clojure
+sudo bash < <(curl -s https://raw.githubusercontent.com/clojure-lsp/clojure-lsp/master/install) 
+
+# bash
+npm i -g bash-language-server  
+
+
+
