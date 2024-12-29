c581d384cf9a4917abd56b3c7e635b72574105f6
Andrii Maliuta <quadr988@gmail.com>
2024-12-29 16:07:30 +0300

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+set fish_greeting ""
+set -U fish_prompt_pwd_dir_length 0
+
+abbr "c" "code"
+abbr "g" "git"
+abbr "gga" "gg amend"
+abbr "ggc" "gg commit"
+abbr "gl" "git log"
+abbr "gp" "git push"
+abbr "gpf" "git push --force-with-lease"
+abbr "gs" "git status"
+abbr "gup" "git pull --rebase"
+abbr "ls" "lsd -l"
+abbr "sw" "gg switch"
+abbr "swd" "git switch --detach"
+abbr "zz" "./zig/zig"
+
+alias "rr" "~/config/tools/remote-run.sh  matklad 192.168.2.4"
+alias "rs" "~/config/tools/remote-sync.sh matklad 192.168.2.4"
+alias "rsh" "ssh matklad@192.168.2.4"
+
+fish_config theme choose termcolors
