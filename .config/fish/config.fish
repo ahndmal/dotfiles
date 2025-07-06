16e3b44d57996cabc59f29ccf4ec2f53075128ec
Andrii Maliuta <quadr988@gmail.com>
2025-07-06 18:40:55 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,24 @@
 set fish_greeting ""
 set -U fish_prompt_pwd_dir_length 0
 
-abbr "c" "code"
+set ATLAS_HOME ""
+
+fish_add_path ~/tools/go/bin
+fish_add_path $ATLAS_HOME/bin
+
 abbr "g" "git"
-abbr "gga" "gg amend"
-abbr "ggc" "gg commit"
 abbr "gl" "git log"
 abbr "gp" "git push"
 abbr "gpf" "git push --force-with-lease"
 abbr "gs" "git status"
 abbr "gup" "git pull --rebase"
-abbr "ls" "lsd -l"
+abbr "ls" "eza -la"
 abbr "sw" "gg switch"
 abbr "swd" "git switch --detach"
 abbr "zz" "./zig/zig"
 
 alias "rr" "~/config/tools/remote-run.sh  andrii 192.168.2.4"
-alias "rs" "~/config/tools/remote-sync.sh andrii 192.168.2.4"
 alias "rsh" "ssh root@192.168.2.4"
+
 
 fish_config theme choose termcolors
